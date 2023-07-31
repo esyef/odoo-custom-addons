d61df35f364b180b713d3357145024fd10479745
esyef_ <yeferson.dev@gmail.com>
2023-07-30 22:19:54 -0500

    chapter 6

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,9 +2,9 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'estate',
-    'version': '1',
-    'category': '',
+    'name': 'Real Estate',
+    'version': '1.0',
+    'category': 'Estate',
     # 'sequence': 15,
     # 'summary': 'Track leads and close opportunities',
     # 'description': "",
@@ -12,44 +12,16 @@
     'depends': [
         'base'
     ],
-    # 'data': [
-    #     'security/crm_security.xml',
-    #     'security/ir.model.access.csv',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
 
-    #     'data/crm_lead_prediction_data.xml',
-    #     'data/crm_lost_reason_data.xml',
-    #     'data/crm_stage_data.xml',
-    #     'data/crm_team_data.xml',
-    #     'data/digest_data.xml',
-    #     'data/mail_data.xml',
-    #     'data/crm_recurring_plan_data.xml',
-
-    #     'wizard/crm_lead_lost_views.xml',
-    #     'wizard/crm_lead_to_opportunity_views.xml',
-    #     'wizard/crm_lead_to_opportunity_mass_views.xml',
-    #     'wizard/crm_merge_opportunities_views.xml',
-
-    #     'views/assets.xml',
-    #     'views/calendar_views.xml',
-    #     'views/crm_recurring_plan_views.xml',
-    #     'views/crm_menu_views.xml',
-    #     'views/crm_lost_reason_views.xml',
-    #     'views/crm_stage_views.xml',
-    #     'views/crm_lead_views.xml',
-    #     'views/digest_views.xml',
-    #     'views/mail_activity_views.xml',
-    #     'views/res_config_settings_views.xml',
-    #     'views/res_partner_views.xml',
-    #     'views/utm_campaign_views.xml',
-    #     'report/crm_activity_report_views.xml',
-    #     'report/crm_opportunity_report_views.xml',
-    #     'views/crm_team_views.xml',
-    # ],
-    # 'demo': [
-    #     'data/crm_team_demo.xml',
-    #     'data/mail_activity_demo.xml',
-    #     'data/crm_lead_demo.xml',
-    # ],
+        # 'demo': [
+        #     'data/crm_team_demo.xml',
+        #     'data/mail_activity_demo.xml',
+        #     'data/crm_lead_demo.xml',
+    ],
     # 'css': ['static/src/css/crm.css'],
     'installable': True,
     'application': True,
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,18 +1,20 @@
-from odoo import fields, models
+from odoo import models, fields
+from datetime import date, timedelta
 
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
-    _description = 'Estate property'
-    _order = 'sequence'
+    _description = 'Real estate property'
 
     name = fields.Char(required=True)
+    active = fields.Boolean('Active', default=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    date_availability = fields.Char(
+        readonly=True, default=lambda self: date.today() + timedelta(days=90))
+    expected_price = fields.Float(readonly=True)
+    selling_price = fields.Float(readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -20,7 +22,12 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         string='Orientation',
-        selection=[('north', 'North'), ('south', 'South'),
-                   ('east', 'East'), ('west', 'West')],
-        help='Select an orientation type'
+        selection=[('North', 'North'), ('South', 'South'),
+                   ('East', 'East'), ('West', 'West')],
+        help='Orientation is used to separate orientation types'
     )
+    state = fields.Selection([
+        ('New', 'New'),
+        ('Offer Received', 'Offer Received'),
+        ('Offer Accepted', 'Offer Accepted'), ('Sold', 'Sold'),  ('Canceled', 'Canceled')], 'State', default='New',
+        store=True)
